2556ed8bba598aa812a326e3ef39527492d5e0ad
fine-pine <lsm3645@g.skku.edu>
2024-12-20 17:31:06 +0900

    feat: DelegatingPasswordEncoder 등록

--- src/main/java/kr/ontherec/backend/global/config/SpringSecurityConfiguration.java
+++ src/main/java/kr/ontherec/backend/global/config/SpringSecurityConfiguration.java
@@ -16,6 +16,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -71,6 +73,11 @@ public class SpringSecurityConfiguration {
 		);
 
 		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 	@Bean
