80466ec742633667c34e3250f848e9da5b8e3c3b
fine-pine <lsm3645@g.skku.edu>
2024-12-20 17:30:31 +0900

    feat: JWT Encoder, Decoder 설정

--- src/main/java/kr/ontherec/backend/global/config/SpringSecurityConfiguration.java
+++ src/main/java/kr/ontherec/backend/global/config/SpringSecurityConfiguration.java
@@ -2,19 +2,36 @@ package kr.ontherec.backend.global.config;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.*;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.val;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -48,6 +65,47 @@ public class SpringSecurityConfiguration {
 			}
 		}));
 
+		// jwt
+		http.oauth2ResourceServer(
+			oauth2 -> oauth2.jwt(Customizer.withDefaults())
+		);
+
 		return http.build();
+	}
+
+	@Bean
+	public KeyPair keyPair() throws NoSuchAlgorithmException {
+		try {
+			val keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048);
+			return keyPairGenerator.generateKeyPair();
+		} catch (Exception ex) {
+			// TODO: 에러 처리
+			throw new RuntimeException(ex);
+		}
+	}
+
+	@Bean
+	public RSAKey rsaKey(KeyPair keyPair) {
+		return new RSAKey.Builder((RSAPublicKey)keyPair.getPublic())
+			.privateKey(keyPair.getPrivate())
+			.keyID(UUID.randomUUID().toString())
+			.build();
+	}
+
+	@Bean
+	public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+		val jwkSet = new JWKSet(rsaKey);
+		return (jwkSelector, ctx) -> jwkSelector.select(jwkSet);
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder(RSAKey rsaKey) throws JOSEException {
+		return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey()).build();
+	}
+
+	@Bean
+	public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwkSource) {
+		return new NimbusJwtEncoder(jwkSource);
 	}
 }
