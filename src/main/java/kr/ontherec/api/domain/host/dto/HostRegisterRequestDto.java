6dd6a7e9ecf1a0fd2aff5106ff0b38b966c70651
fine-pine <lsm3645@g.skku.edu>
2025-02-15 00:02:13 +0900

    refactor: 정규표현식 Regex 클래스로 분리

--- src/main/java/kr/ontherec/api/domain/host/dto/HostRegisterRequestDto.java
+++ src/main/java/kr/ontherec/api/domain/host/dto/HostRegisterRequestDto.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import kr.ontherec.api.domain.host.domain.Bank;
 
+import static kr.ontherec.api.global.model.Regex.BANK_ACCOUNT;
+
 public record HostRegisterRequestDto(
         @NotNull(message = "은행을 입력해주세요")
         Bank bank,
         @NotBlank(message = "계좌번호를 입력해주세요")
-        @Pattern(regexp = "d{8,14}", message = "유효하지 않은 계좌번호 입니다")
+        @Pattern(regexp = BANK_ACCOUNT, message = "유효하지 않은 계좌번호 입니다")
         String account
 ) {
 }
--- src/main/java/kr/ontherec/api/domain/host/dto/HostUpdateRequestDto.java
+++ src/main/java/kr/ontherec/api/domain/host/dto/HostUpdateRequestDto.java
@@ -5,9 +5,11 @@ import kr.ontherec.api.domain.host.domain.Bank;
 
 import java.time.LocalTime;
 
+import static kr.ontherec.api.global.model.Regex.BANK_ACCOUNT;
+
 public record HostUpdateRequestDto(
         Bank bank,
-        @Pattern(regexp = "d{8,14}", message = "유효하지 않은 계좌번호 입니다")
+        @Pattern(regexp = BANK_ACCOUNT, message = "유효하지 않은 계좌번호 입니다")
         String account,
         LocalTime contactFrom,
         LocalTime contactUntil
