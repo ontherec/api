7f66e19f52839e9feb854e0e5ed4553aadbd742f
이송목 <lsm3645@g.skku.edu>
2025-03-23 15:36:09 +0900

    feat: 이미지 및 파일 업로드 URL 발급 API 구현 (#21)
    * feat: S3Service 구현
    
    * feat: S3Controller 구현
    
    * feat: S3Exception & Code 선언
    
    * feat: FileExtension 선언
    
    * feat: 매개변수로 파일 확장자를 주입 받도록 수정
    
    * ci: CI/CD 환경 변수 설정

---
+++ src/main/java/kr/ontherec/api/infra/bucket/FileExtension.java
@@ -0,0 +1,36 @@
+package kr.ontherec.api.infra.bucket;
+
+import kr.ontherec.api.infra.bucket.exception.S3Exception;
+import kr.ontherec.api.infra.bucket.exception.S3ExceptionCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+public enum FileExtension {
+    IMAGE_PNG("png", "images/o", "image/png"),
+    IMAGE_JPG("jpg", "images/o", "image/jpeg"),
+    IMAGE_JPEG("jpeg", "images/o", "image/jpeg"),
+    IMAGE_WEBP("webp", "images/o", "image/webp"),
+    IMAGE_AVIF("avif", "images/o", "image/avif"),
+
+    DOCS_HWP("hwp", "docs", "application/octet-stream"),
+    DOCS_HWPX("hwpx", "docs", "application/octet-stream"),
+    DOCS_DOC("doc", "docs", "application/msword"),
+    DOCS_DOCX("docx", "docs", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+
+    private final String value;
+    private final String folder;
+    private final String contentType;
+
+    public static FileExtension from(String value) {
+        for (FileExtension ext : values()) {
+            if (value.equals(ext.value)) {
+                return ext;
+            }
+        }
+        throw new S3Exception(S3ExceptionCode.NOT_VALID_FILE_EXTENSION);
+    }
+}
