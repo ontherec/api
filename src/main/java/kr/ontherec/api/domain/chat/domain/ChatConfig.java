cf8094f6a32823bf764ea4776de38edec68d0095
fine-pine <lsm3645@g.skku.edu>
2025-03-27 03:11:03 +0900

    feat: Chat 관련 엔티티 선언

---
+++ src/main/java/kr/ontherec/api/domain/chat/domain/ChatConfig.java
@@ -0,0 +1,10 @@
+package kr.ontherec.api.domain.chat.domain;
+
+public class ChatConfig {
+    public static final Long SYSTEM_ID = 0L;
+    public static final String SYSTEM_USERNAME = "SYSTEM";
+
+    public static final String CHAT_CREATED = "채팅방이 생성되었습니다";
+    public static final String PRACTICE_ROOM_BOOKING_APPROVED = "연습실 예약이 확정되었습니다";
+    public static final String STAGE_BOOKING_APPROVED = "공연장 예약이 확정되었습니다. 마감 기한 내에 큐시트를 제출해주세요";
+}
---
+++ src/main/java/kr/ontherec/api/domain/chat/domain/MessageType.java
@@ -0,0 +1,8 @@
+package kr.ontherec.api.domain.chat.domain;
+
+public enum MessageType {
+    TEXT,
+    IMAGE,
+    FILE,
+    NOTICE
+}
