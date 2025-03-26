8540bebb711a8f90fcfb44f05b812cf6475101c4
fine-pine <lsm3645@g.skku.edu>
2025-03-27 03:12:01 +0900

    feat: Chat & Message Service 인터페이스 선언

---
+++ src/main/java/kr/ontherec/api/domain/chat/application/MessageService.java
@@ -0,0 +1,12 @@
+package kr.ontherec.api.domain.chat.application;
+
+import kr.ontherec.api.domain.chat.domain.Chat;
+import kr.ontherec.api.domain.chat.domain.Message;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface MessageService {
+    List<Message> getPageByChatId(Long chatId, Pageable pageable);
+    void send(Chat chat, Message message);
+}
