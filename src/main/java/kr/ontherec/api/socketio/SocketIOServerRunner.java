cb7ce72e9d3d04cd08ae0706d97bdd20a3589de3
fine-pine <lsm3645@g.skku.edu>
2025-03-29 17:15:21 +0900

    test: socketio 테스트 제외

--- src/main/java/kr/ontherec/api/socketio/SocketIOServerRunner.java
+++ src/main/java/kr/ontherec/api/socketio/SocketIOServerRunner.java
@@ -3,9 +3,11 @@ package kr.ontherec.api.socketio;
 import com.corundumstudio.socketio.SocketIOServer;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile({ "local", "dev" })
 public class SocketIOServerRunner {
     private final SocketIOServer server;
 
