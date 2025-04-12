30d7e24f5a84054445d6a97659f3ee6878d99528
fine-pine <lsm3645@g.skku.edu>
2025-04-12 15:36:43 +0900

    feat: StopWatch 어노테이션 구현

---
+++ src/main/java/kr/ontherec/api/infra/util/StopWatch.java
@@ -0,0 +1,11 @@
+package kr.ontherec.api.infra.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(METHOD)
+@Retention(RUNTIME)
+public @interface StopWatch {}
